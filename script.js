f4f274f957c6efcff87263db4ceb584a3c5ec2ec
LMulller17 <loloomuller17@gmail.com>
2024-05-28 16:54:44 -0300

    trabajo final

---
+++ script.js
@@ -0,0 +1,7 @@
+//Consultar si es mayor de edad para ingresar
+let consulta = prompt ("¿cuantos años tienes?");
+if (consulta >= 18){
+    alert("Bienvenido a Drinkedin_ ");
+} else {
+    alert("No puedes ingresar");
+}
