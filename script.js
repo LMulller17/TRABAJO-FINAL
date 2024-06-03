878ca974e8af0608b017296587270d72ce5e64f0
LMulller17 <loloomuller17@gmail.com>
2024-06-03 16:50:41 -0300

    agregado de cartel

--- script.js
+++ script.js
@@ -1,7 +1,69 @@
 //Consultar si es mayor de edad para ingresar
-let consulta = prompt ("¿cuantos años tienes?");
+/*let consulta = prompt ("¿cuantos años tienes?");
 if (consulta >= 18){
     alert("Bienvenido a Drinkedin_ ");
 } else {
     alert("No puedes ingresar");
-}
+}*/
+
+Swal.fire({
+	 title: "Bienvenido",
+	 text: "Confirmar si eres mayor de edad",
+	// html:
+	 icon: "warning",
+	 confirmButtonText: "Confirmar",
+	 footer: "Estas es informacion importante",
+	 width: "90%",
+	 padding: "1rem",
+	// background: "blue",///////
+	// grow:
+	 backdrop: "true",
+	// timer:
+	// timerProgressBar:
+	// toast:
+	 position: "center",
+	 allowOutsideClick: "false",
+	 allowEscapeKey: "false",
+	 allowEnterKey: "false",
+	 stopKeydownPropagation: "false",
+
+	// input:
+	// inputPlaceholder:
+	// inputValue:
+	// inputOptions:
+	
+	 //customClass:
+	 	//container:
+	 	//popup:
+	 	//header:
+	 	//title:
+	 	//closeButton:
+	 	//icon:
+	 	//image:
+	 	//content:
+	 	//input:
+	 	//actions:
+	 	//confirmButton:
+	 	//cancelButton:s
+	 	//footer:	
+     
+
+	 showConfirmButton: "true",
+	// confirmButtonColor:
+	 confirmButtonAriaLabel: "Confirtmar",
+
+	 showCancelButton: "true",
+	 cancelButtonText: "Cancelar",
+	// cancelButtonColor:
+	 cancelButtonAriaLabel: "Cancelar",
+	
+	// buttonsStyling:
+	// showCloseButton:
+	// closeButtonAriaLabel:
+
+
+	//imageUrl: 
+	//imageWidth: ,
+	// imageHeight:
+	// imageAlt: 
+});
