55489834ed703691c98b405c1cea4da5c2d2b405
Lorenzo Muller <166562355+LMulller17@users.noreply.github.com>
2024-09-11 04:31:45 -0300

    TERMINADO AL FIN

---
+++ javascript/script.js
@@ -0,0 +1,66 @@
+
+Swal.fire({
+    title: "Bienvenido",
+    text: "Estas intereado en venir a conocernos?",
+   // html:
+    icon: "warning",
+    confirmButtonText: "Confirmar",
+    footer: "Esta es informacion importante",
+    width: "90%",
+    padding: "1rem",
+   // background: "blue",///////
+    grow: "row",
+    backdrop: true,
+   // timer:
+   // timerProgressBar:
+   // toast:
+    position: "center",
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    allowEnterKey: false,
+    stopKeydownPropagation: false,
+
+   // input:
+   // inputPlaceholder:
+   // inputValue:
+   // inputOptions:
+
+    //customClass:
+        //container:
+        //popup:
+        //header:
+        //title:
+        //closeButton:
+        //icon:
+        //image:
+        //content:
+        //input:
+        //actions:
+        //confirmButton:
+        //cancelButton:s
+        //footer:	
+    
+
+    showConfirmButton: "true",
+    confirmButtonColor: "black",
+    confirmButtonAriaLabel: "Confirtmar",
+
+    showCancelButton: "true",
+    cancelButtonText: "Cancelar",
+   // cancelButtonColor:
+    cancelButtonAriaLabel: "Cancelar",
+
+   // buttonsStyling:
+   // showCloseButton:
+   // closeButtonAriaLabel:
+
+
+   //imageUrl: 
+   //imageWidth: ,
+   // imageHeight:
+   // imageAlt: 
+});
+
+
+
+
